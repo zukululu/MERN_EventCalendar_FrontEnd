03fae6d0e09c6b96fd50e674c3abad746318b565
Sun-Mountain <npzonnenberg@gmail.com>
2019-02-19 12:07:12 -0500

    calendar added to mp

--- mern-calendar-app/src/components/MainEvent/MainEvent.js
+++ mern-calendar-app/src/components/MainEvent/MainEvent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Calendar from 'react-calendar/dist/entry.nostyle';
+import './MainEvent.scss';
 
 class MainEvent extends Component {
     render() {
         return (
-            <div>
-                Main Event ~ta da~
+            <div className="main-event">
+                <Calendar />
             </div>
         );
     }
