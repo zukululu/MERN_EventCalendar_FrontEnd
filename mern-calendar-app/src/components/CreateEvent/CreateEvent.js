357d9283420bb7367f8754427bcf023dea501921
Sun-Mountain <npzonnenberg@gmail.com>
2019-02-21 09:30:42 -0500

    caught up

--- mern-calendar-app/src/components/CreateEvent/CreateEvent.js
+++ mern-calendar-app/src/components/CreateEvent/CreateEvent.js
@@ -1,36 +1,80 @@
 import React, { Component } from 'react';
+import DatePicker from "react-datepicker";
 import './CreateEvent.scss';
+import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios'
 
 class CreateEvent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            date: new Date(),
+            author: '',
+            public: false
+        };
+
+        this.handleDateChange = this.handleDateChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    handleDateChange(date) {
+        this.setState({
+            date: date
+        });
+    }
+
+    handleChange(evt) {
+        const variable = evt.target.name
+        console.log(variable)
+        this.setState({[variable]: evt.target.value }, 
+        () => {
+            console.log(this.state)
+        })
+        // console.log(evt.target.name)
+        // console.log(evt.target.value)
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+        axios.post('http://localhost:3001/events/new', this.state)
+            .then(res => {
+                console.log(res.data)
+            })
+    }
+
     render() {
         return (
             <div className="create-form">
-                <h2>New Event</h2>
+                <div className="create-form-header">
+                    <h2>New Event</h2>
+                </div>
 
-                <form>
-                    <div>
-                        <label htmlFor='title'>Event Name</label>
-                        <input type="text" name="title" className="form form_title" />
-                    </div>
-                    <div>
+                <form className="form-wrapper" onSubmit={this.handleSubmit}>
+                    <div className="label-title"><label htmlFor='title'>Event Name</label></div>
+                    <div className="form-title"><input type="text" name="title" value={this.state.value} className="form" onChange={this.handleChange} /></div>
+                    <div className="calendar"></div>
+                    <div className="date">
+                        <label htmlFor='date'>Date</label>
+                        <DatePicker
+                            selected={this.state.date}
+                            onChange={this.handleDateChange} name="date" value={this.state.value}
+                        /></div>
+                    <div className="location">
                         <label htmlFor='location'>Location</label>
-                        <input type="text" name="location" className="form form_location" />
+                        <input type="text" name="location" value={this.state.value} className="form" onChange={this.handleChange} /></div>
+                    <div className="label-description"><label htmlFor='description'>Description</label></div>
+                    <div className="form-description">
+                        <textarea type="text" name="description" value={this.state.value} className="form" onChange={this.handleChange} />
                     </div>
-                    <div>
-                        <label htmlFor='description'>Description</label>
-                    </div>
-                    <div>
-                        <textarea type="text" name="description" className="form form_description" />
-                    </div>
-                    <div>
-                        <label htmlFor='cover'>Cover?</label>
-                        <input type="text" name="cover" className="form form_cover" />
-                    </div>
-                    <div>
-                        <label htmlFor='public'>Public Event</label>
-                        <input type="checkbox" name="public" />
-                    </div>
-                    <input value='Submit' type='submit' className="button" />
+                    <div className="label-cover"><label htmlFor='cover'>Cover?</label></div>
+                    <div className="form-cover"><input type="text" name="cover" value={this.state.value} className="form" onChange={this.handleChange} /></div>
+                    <div className="label-public"><label htmlFor='public'>Public Event</label></div>
+                    <div className="form-public"><input type="checkbox" name="public" value={this.state.value} onChange={this.handleChange} /></div>
+                    <div className="button-submit"><input value='Submit' type='submit' className="button" /></div>
                 </form>
             </div>
         );
