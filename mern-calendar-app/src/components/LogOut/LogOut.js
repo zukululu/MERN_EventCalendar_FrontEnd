1c04376c7ecfac9594de8b32bd206a17f08a2eae
Sun-Mountain <npzonnenberg@gmail.com>
2019-02-21 13:40:34 -0500

    logout redirect

--- mern-calendar-app/src/components/LogOut/LogOut.js
+++ mern-calendar-app/src/components/LogOut/LogOut.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
 import './Logout.scss'
 
 class LogOut extends Component {
+
+    handleRedirect() {
+        return <Redirect to='/' />
+    }
+
     render() {
         return (
             <div className="button-wrapper">
                 <form>
                     <input value='Log Out' type='submit' onClick={this.props.handleLogOut} className="button-logout" />
                 </form>
+                {this.props.isLoggedIn === false ? this.handleRedirect() : null}
             </div>
         );
     }
